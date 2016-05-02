5c61698d9f50160dd5c8addfddf9bfd620c4cbb9
HappyEricChen <chenzhihao.exe@gmail.com>
2016-05-02 15:35:29 +0800

    add basic for star App

---
+++ star/star/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  star
+//
+//  Created by Eric Chen on 16/5/2.
+//  Copyright © 2016年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ star/star/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  star
+//
+//  Created by Eric Chen on 16/5/2.
+//  Copyright © 2016年 Eric. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HomeViewController : BaseViewController
+
+@end
