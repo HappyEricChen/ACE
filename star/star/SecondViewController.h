c33f37ff62c4a7ef25b81e3c3fea5cc043890f41
HappyEricChen <chenzhihao.exe@gmail.com>
2016-05-09 14:50:47 +0800

    add SDAutoLayout & QQPlist

---
+++ star/star/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  star
+//
+//  Created by Eric Chen on 16/5/9.
+//  Copyright © 2016年 Eric. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SecondViewController : BaseViewController
+
+@end
---
+++ star/star/SecondViewController.m
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.m
+//  star
+//
+//  Created by Eric Chen on 16/5/9.
+//  Copyright © 2016年 Eric. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
